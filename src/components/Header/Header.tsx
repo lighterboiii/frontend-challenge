a2c9c5436ef32267b09d2977e0f87e79dcb54554
Vyacheslav Baumtrok <s.baumtrok2009@yandex.ru>
2024-01-30 17:10:39 +0300

    minor style fix

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,8 +5,10 @@ import './Header.scss';
 const Header: FC = () => {
   return (
     <div className='header'>
-      <Link to='/' className='header__link'>Все котики</Link>
-      <Link to='/favourites' className='header__link'>Любимые котики</Link>
+      <div className='header__links'>
+        <Link to='/' className='header__link'>Все котики</Link>
+        <Link to='/favourites' className='header__link'>Любимые котики</Link>
+      </div>
     </div>
   )
 };
